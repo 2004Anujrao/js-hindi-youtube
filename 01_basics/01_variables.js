95d4f5ca14bee065c04220b795ae5cb74fcb9ed2
2004Anujrao <raoanuj28@gmail.com>
2025-02-21 12:37:50 +0000

    class 01_variable on js-hindi-youtube

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 144553
+let accountEmail = "ay1157080@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+var number223;
+// const jhdsg435;  this condition is not allowed on cont it can be only let and var
+// accountId = 423  not allowed
+
+
+ accountEmail = "jh@gmail.com"
+accountPassword = "475"
+accountCity = "Delhi"
+
+console.log(accountId);
+
+accountEmail = "fhdsg@gmail.com"
+
+/*
+prefer not to use var
+because of issue in block scop and functional scope
+*/
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState,number223])
