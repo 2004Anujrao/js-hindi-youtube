fa8a656947282cfc5ae0adf515abc4b9669bb62a
2004Anujrao <raoanuj28@gmail.com>
2025-06-22 09:55:39 +0000

    conversion and operation statement

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,65 @@
+//let score = 33
+
+//console.log(typeof score);
+//console.log(typeof (score))
+//let valueInNumber = Number(score);
+
+//console.log(typeof valueInNumber)
+
+// "33" => 33
+// "33abc" =>NaN
+//true => 1; false => 0
+
+/*let isLoggedIn = ""
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+*/
+/* 1 => true ;  ,  0 => false;
+" " = false
+"Anuj" = True*/
+
+let someNumber = 26
+let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof someNumber)
+// console.log(typeof stringNumber)
+
+
+
+// ------------------Operations------------------------
+let value = 5
+let negvalue = -value
+// console.log(negvalue)
+// console.log(2+2);
+// console.log(5-3);
+// console.log(2*3);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = "Anuj"
+let str3 = str1 + str2
+console.log(str3)
+
+
+// console.log("1"+2);     // output => 12
+// console.log(1 + "2");      // 12
+// console.log("1"+2+3);      //123
+// console.log(1+2+"3");      //33
+
+// console.log(true) // true
+// console.log(+true);   // 1
+// console.log(+"");    //0
+
+
+let num1 , num2 , num3 
+
+num1 = num2 = num3 = 2 + 2 // don't become that much oversmart
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
