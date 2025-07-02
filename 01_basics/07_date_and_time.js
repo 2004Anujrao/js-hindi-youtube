6d80c532e6edd3d542639d210e4499f4e77dfbd4
2004Anujrao <raoanuj28@gmail.com>
2025-07-02 06:50:34 +0000

    date and time

---
+++ 01_basics/07_date_and_time.js
@@ -0,0 +1,22 @@
+
+
+// let myDate = new Date()
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let mycreatedDate = new Date(2050 , 0 , 25)
+// console.log(mycreatedDate.toString());
+
+
+// console.log()
+let mycreatedDate = new Date("01-14-2023")
+
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(mycreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
